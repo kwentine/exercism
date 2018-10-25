7e91d7f690bb971167ba8ada31d101604e756f09
Quentin Agren <quentin.agren@sciencespo.fr>
2018-10-25 10:03:38 +0200

    [py] Refactor Garden class

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -17,26 +17,36 @@ class Garden(object):
 
     
     def __init__(self, diagram, students=None):
-
         if students:
-            self.students =  sorted(students)
-
-        front_row, back_row = diagram.split('\n')
-
-        # Check that the diagram is valid
-        allowed_species = ''.join(self.plant_species)
-        spec = r'^[%s]{%s}$' % (allowed_species, len(self.students) * 2)
-        if not re.match(spec, front_row) and re.match(spec, back_row):
-            raise ValueError('Invalid diagram.')
-        
-        # Form a list of strings representing student lots:
-        #   - generate pairs of consecutive letters for each row
-        #   - zip togeter the corresponding tuples
-        #   - concatenate
-        i1, i2 = iter(front_row), iter(back_row)
-        plant_lots = [a + b + c + d for (a, b), (c, d) in zip(zip(i1, i1), zip(i2, i2))]
-
+            self.students =  sorted(students)        
+        front_row, back_row = self._validate_diagram(diagram)
+        plant_lots = self._get_plant_lots(front_row, back_row)
         self.student_lots = dict(zip(self.students, plant_lots))
 
     def plants(self, student):
         return [self.plant_species[p] for p in list(self.student_lots[student])]
+
+    def _validate_diagram(self, diagram):
+        """Check that the diagram is valid
+
+        Raise ValueError if the diagram is invalid.
+        Return (front_row, back_row) pair otherwise.
+        """
+        allowed_species = ''.join(self.plant_species)
+        spec = f'^[{allowed_species}]+\n[{allowed_species}]+$'
+        if not re.match(spec, diagram):
+            raise ValueError('Invalid diagram.')
+        return tuple(diagram.split('\n'))
+
+    def _get_plant_lots(self, front_row, back_row):
+        """Return a list of strings representing consecutive student lots
+        
+        This is done in three steps:
+          - generate pairs of consecutive letters for each row
+          - zip togeter the corresponding tuples
+          - concatenate
+        """
+        i1, i2 = iter(front_row), iter(back_row)
+        return [a + b + c + d for (a, b), (c, d) in zip(zip(i1, i1), zip(i2, i2))]
+        
+        

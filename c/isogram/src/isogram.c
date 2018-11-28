d69cb9756e70b3ff18eac720647db0a9558b160f
Quentin Agren <quentin.agren@sciencespo.fr>
2018-11-28 05:00:24 +0100

    [c] Isogram iteration 2

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -1,9 +1,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
+#include <ctype.h>
 #define LOWER_A 'a'
 #define UPPER_A 'A'
-#define LOWER_Z 'z'
-#define UPPER_Z 'Z'
 #define ALPHABET_LENGTH 26
 /* is_isogram: check wether a phrase has no repeating letter
 
@@ -13,22 +12,16 @@
    is the case for ASCII.
 */
 bool is_isogram(const char phrase[]) {
-  int i = 0;
+  int i;
   char c;
-  int seen[ALPHABET_LENGTH];
+  int seen[ALPHABET_LENGTH] = {0};
 
   if (phrase == NULL)
     return 0;
   
-  // Initialize the array of characters seen so far
-  for (i = 0; i < ALPHABET_LENGTH; seen[i++] = 0)
-    ;
+  for (i = 0; (c = phrase[i]) != '\0'; i++)
+    if ((islower(c) && seen[c - LOWER_A]++) || (isupper(c) && seen[c - UPPER_A]++))
+      return 0;
 
-  i = 0;
-  while((c = phrase[i++]))
-    if (c >= LOWER_A && c <= LOWER_Z && seen[c - LOWER_A]++)
-      return 0;
-    else if (c >= UPPER_A && c <= UPPER_Z && seen[c - UPPER_A]++)
-      return 0;
   return 1;
 }

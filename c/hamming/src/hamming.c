ca9b07038178937593e26020c8beb9773757438c
Quentin Agren <quentin.agren@sciencespo.fr>
2018-12-06 16:53:01 +0100

    [c] Hamming second iteration

--- c/hamming/src/hamming.c
+++ c/hamming/src/hamming.c
@@ -1,15 +1,17 @@
-int compute(char s[], char t[]) {
-  int i, h;
+int compute(char left_str[], char right_str[]) {
+  int i;
+  int hamming_dist = 0;
 
   // Check for NULL pointers
-  if (!s || !t)
+  if (!left_str || !right_str)
     return -1;
   
-  for (i = 0, h = 0; s[i] != '\0' && t[i] != '\0'; i++) {
-    if (s[i] != t[i])
-      h++;
+  for (i = 0; left_str[i] != '\0' && right_str[i] != '\0'; i++) {
+    if (left_str[i] != right_str[i])
+      hamming_dist++;
   }
 
-  // Return h unless input strings are of different size
-  return (s[i] != t[i]) ? -1 : h;
+  // Return hamming distance, unless we haven't reached the end of
+  // both input strings in which case it is an error.
+  return (left_str[i] == '\0' && right_str[i] == '\0') ? hamming_dist : -1;
 }
